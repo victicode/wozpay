28575af3e9e3681f819bb373a6408d862ccf98e8
Victor Rivero <frovic.ve@gmail.com>
2024-06-16 11:52:01 -0400

    avances en login

--- resources/js/src/pages/login.vue
+++ resources/js/src/pages/login.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
-    hola aqui en login
+    <div style="height: 100vh">
+      <div class="bg-terciary row flex-center" style="height:20%" >
+        <h2 class="text-white" style="font-family: arial;">Woz Pay</h2>
+      </div>
+      <div style="height:80%">
+
+      </div>
+    </div>
   </div>
 </template>
