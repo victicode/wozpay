327315c3851298fa791291b6c1e40de8728f99ce
Victor Rivero <VRIVERO@forum.local>
2024-12-11 15:38:00 -0400

    avances

--- resources/js/src/pages/linked_card.vue
+++ resources/js/src/pages/linked_card.vue
@@ -16,7 +16,7 @@
               <q-icon :name="icons.outlinedAddCard" size="lg" color="primary" />
             </div>
             <div class="q-px-md text-center q-py-sm q-mt-sm infoCard text-subtitle2">
-              Te debitaremos una pequeña tarifa para <br> validar la tarjeta
+              Te debitaremos una tarifa 2 USD ~ (Gs. 15.628) para <br> validar la tarjeta
             </div>
             <div class="q-my-lg">
               <q-input
