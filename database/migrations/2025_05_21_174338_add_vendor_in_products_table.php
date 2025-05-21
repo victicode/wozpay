10d2f0f9bbff84af5ba81601108e670cab87d75d
Victor Rivero <VRIVERO@forum.local>
2025-05-21 15:42:23 -0400

    avacnes

---
+++ database/migrations/2025_05_21_174338_add_vendor_in_products_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            //
+            $table->unsignedBigInteger('vendor_id')->nullable()->after('categorie_id');
+            $table->foreign('vendor_id')->references('id')->on('vendors')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            //
+        });
+    }
+};
--- resources/js/src/components/dropshipping/product/listSquareProducts.vue
+++ resources/js/src/components/dropshipping/product/listSquareProducts.vue
@@ -1,5 +1,27 @@
 <template>
   <div>
-    
+    <div>
+      <img :src="product.image" alt="">
+    </div>
+    <div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
   </div>
 </template>
+<script>
+export default {
+  props: {
+    product: Object,
+  },
+  emits: ['getPay'],
+  setup (props, { emit }) {
+    return{
+      product: props.product
+    }
+  }
+}
+</script>
